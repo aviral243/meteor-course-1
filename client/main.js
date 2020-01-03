3a25469fddc48b580b7c233ca6253c4dd106c298
aviral243 <aviralgangwar24@gmail.com>
2020-01-04 04:38:18 +0530

    Init commit

---
+++ client/main.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>Hello there!</div>;
+};
+
+Meteor.startup(() => {
+  ReactDOM.render(<App />, document.querySelector(".container"));
+});
